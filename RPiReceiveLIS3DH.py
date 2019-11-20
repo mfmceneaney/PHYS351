3dbfe9d5db8a20d1de2d5e9ab0d45d9158618f2f
mfmceneaney <50933545+mfmceneaney@users.noreply.github.com>
2019-11-20 13:48:52 -0500

    Update RPiReceiveLIS3DH.py

--- RPiReceiveLIS3DH.py
+++ RPiReceiveLIS3DH.py
@@ -1,9 +1,9 @@
 import bluetooth
  
-bd_addr = "98:D3:31:50:0A:CE" //The address from the HC-05 sensor
+bd_addr = "E6:52:28:5E:0C:53" #The address from the Adafruit Bluefruit LE SPI Friend
 port = 1
 sock = bluetooth.BluetoothSocket (bluetooth.RFCOMM)
-sock.connect((bd_addr,port))
+sock.connect((bd_addr,port)) # Try pairing first to avoid host down error?
  
 data = ""
 while 1:

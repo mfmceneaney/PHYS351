d3b6cac44df22c78d5f1033e9949b884033c722c
mfmceneaney <50933545+mfmceneaney@users.noreply.github.com>
2019-12-03 09:22:19 -0500

    Create bluepy_installation.sh

---
+++ bluepy_installation.sh
@@ -0,0 +1,16 @@
+[bluetooth]# info E4:DB:75:D5:D2:AC
+Device E4:DB:75:D5:D2:AC
+	Name: Adafruit Bluefruit LE
+	Alias: Adafruit Bluefruit LE
+	Paired: yes
+	Trusted: yes
+	Blocked: no
+	Connected: no
+	LegacyPairing: no
+	UUID: Vendor specific           (00001530-1212-efde-1523-785feabcd123)
+	UUID: Generic Access Profile    (00001800-0000-1000-8000-00805f9b34fb)
+	UUID: Generic Attribute Profile (00001801-0000-1000-8000-00805f9b34fb)
+	UUID: Device Information        (0000180a-0000-1000-8000-00805f9b34fb)
+	UUID: Vendor specific           (6e400001-b5a3-f393-e0a9-e50e24dcca9e)
+	RSSI: -51
+	TxPower: 0

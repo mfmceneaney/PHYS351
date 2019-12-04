8d68bce39960b6397171ead756a808aded404f14
mfmceneaney <50933545+mfmceneaney@users.noreply.github.com>
2019-12-04 08:53:37 -0500

    Update ArduinoTransmitLIS3DH.c
    Minimized data sent over bluetooth connection.  Also added note to make sure UART Friend is in UART mode not CMD mode (flip switch on board) to send data over bluetooth.

--- ArduinoTransmitLIS3DH.c
+++ ArduinoTransmitLIS3DH.c
@@ -1,3 +1,11 @@
+// General Imports
+#include <Arduino.h>
+#include <SPI.h>
+#include <SoftwareSerial.h>
+//  #include <SoftwareSerial.h>
+//#endif
+
+// Adafruit Bluefruit LE Imports
 #include <Adafruit_ATParser.h>
 #include <Adafruit_BluefruitLE_SPI.h>
 #include <Adafruit_BLEMIDI.h>
@@ -6,37 +14,12 @@
 #include <Adafruit_BLEEddystone.h>
 #include <Adafruit_BLE.h>
 #include <Adafruit_BluefruitLE_UART.h>
-
-#include <SoftwareSerial.h>
-
-/*********************************************************************
- This is an example for our nRF51822 based Bluefruit LE modules
-
- Pick one up today in the adafruit shop!
-
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
-
- MIT license, check LICENSE for more information
- All text above, and the splash screen below must be included in
- any redistribution
-*********************************************************************/
-
-#include <Arduino.h>
-#include <SPI.h>
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BluefruitLE_UART.h"
-
 #include "BluefruitConfig.h"
 
-#if SOFTWARE_SERIAL_AVAILABLE
-  #include <SoftwareSerial.h>
-#endif
-
-// Basic demo for accelerometer readings from Adafruit LIS3DH
-
+// Adafruit LIS3DH Imports
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_LIS3DH.h>
@@ -55,9 +38,6 @@
 //Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
 // I2C
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
-
-// MFM 11/16/19
-SoftwareSerial lisSerial(LIS3DH_MISO, LIS3DH_MOSI); // RX, TX
 
 // MFM 11/18/19
 //SoftwareSerial Serial(BLUEFRUIT_SPI_MISO, BLUEFRUIT_SPI_MOSI); // RX, TX
@@ -95,7 +75,7 @@ SoftwareSerial lisSerial(LIS3DH_MISO, LIS3DH_MOSI); // RX, TX
 
 // Create the bluefruit object, either software serial...uncomment these lines
 
-SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN); //, 
 
 Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
                       BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
@@ -115,119 +95,141 @@ Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
 
 // A small helper
 void error(const __FlashStringHelper*err) {
+  Serial.println(err);
   bluefruitSS.println(err);
   while (1); 
 }
 
 /**************************************************************************/
 /*!
-    @brief  Sets up the HW an the BLE module (this function is called
+    @brief  Sets up the HW an the BLE module and the LIS3DH (this function is called
             automatically on startup)
 */
 /**************************************************************************/
 void setup(void)
 {
-  //while (!Serial);  // required for Flora & Micro
-  //delay(500);
-
   // Arduino Serial Monitor comes with a 115200 baud rate.
   Serial.begin(115200);
   // RPi bluetooth comes automatically configured to a 9600 baud rate.
   bluefruitSS.begin(9600);
-  Serial.println(F("Adafruit Bluefruit AT Command Example"));
-  bluefruitSS.println(F("Adafruit Bluefruit AT Command Example"));
-  Serial.println(F("-------------------------------------"));
 
-  /* Initialise the module */
+  //=======================================================================================//
+  /* Initialise the Bluefruit module */
   Serial.print(F("Initialising the Bluefruit LE module: "));
-
-  if ( !ble.begin(VERBOSE_MODE) )
-  {
-    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
-  }
+//  bluefruitSS.print(F("Initialising the Bluefruit LE module: "));
+  
+  // These lines have been returning an error
+  //if ( !ble.begin(VERBOSE_MODE) )
+  //{
+  //  error(F("Couldn't find Bluefruit, make sure it's in Command mode & check wiring?"));
+  //}
+  
   Serial.println( F("OK!") );
-
-  if ( FACTORYRESET_ENABLE )
-  {
-    /* Perform a factory reset to make sure everything is in a known state */
-    Serial.println(F("Performing a factory reset: "));
-    bluefruitSS.println(F("Performing a factory reset: "));
-    if ( ! ble.factoryReset() ){
-      error(F("Couldn't factory reset"));
-    }
-  }
+//  bluefruitSS.println( F("OK!") );
+  
+  // Factory Reset has not been working
+  //if ( FACTORYRESET_ENABLE )
+  //{
+  //  /* Perform a factory reset to make sure everything is in a known state */
+  //  Serial.println(F("Performing a factory reset: "));
+  //  bluefruitSS.println(F("Performing a factory reset: "));
+  //  if ( ! ble.factoryReset() ){
+  //    error(F("Couldn't factory reset"));
+  //  }
+  //}
+  
 
   /* Disable command echo from Bluefruit */
   ble.echo(false);
 
   Serial.println("Requesting Bluefruit info:");
-  bluefruitSS.println("Requesting Bluefruit info:");
-  /* Print Bluefruit information */
+//  bluefruitSS.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information through bluefruit module */
   ble.info();
   //=======================================================================================//
   // from acceldemo.c 
   while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until Serial console opens
 
   Serial.println("LIS3DH test!");
+//  bluefruitSS.println("LIS3DH test!");
   
   if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
     Serial.println("Couldnt start");
+//    bluefruitSS.println("Couldnt start");
     while (1) yield();
   }
   Serial.println("LIS3DH found!");
+//  bluefruitSS.println("LIS3DH found!");
   
   lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
   
   Serial.print("Range = "); Serial.print(2 << lis.getRange());  
   Serial.println("G");
+//  bluefruitSS.print("Range = "); bluefruitSS.print(2 << lis.getRange());  
+//  bluefruitSS.println("G");
   //=======================================================================================//
 }
 
 /**************************************************************************/
 /*!
-    @brief  Constantly poll for new command or response data
+    @brief  Constantly poll for new command and transmit acceleration measurements 
 */
 /**************************************************************************/
 void loop(void)
 {
   // Display command prompt
-  Serial.print(F("AT > "));
-  bluefruitSS.print(F("AT > "));
+  //Serial.print(F("AT > "));
+  //bluefruitSS.print(F("AT > "));
 
   // Check for user input and echo it back if anything was found
-  char command[BUFSIZE+1];
-  getUserInput(command, BUFSIZE);
+  //char command[BUFSIZE+1];
+  //getUserInput(command, BUFSIZE);
 
   // Send command
-  ble.println(command);
+  //ble.println(command);
 
   // Check response status
-  ble.waitForOK();
+  //ble.waitForOK();
 
   lis.read();      // get X Y and Z data at once
-  // Then print out the raw data
-  bluefruitSS.print("X:  "); bluefruitSS.print(lis.x); 
-  bluefruitSS.print("  \tY:  "); bluefruitSS.print(lis.y); 
-  bluefruitSS.print("  \tZ:  "); bluefruitSS.print(lis.z);
   
-  // Print measurements to serial monitor
-  Serial.print("X:  "); Serial.print(lis.x); 
-  Serial.print("  \tY:  "); Serial.print(lis.y); 
-  Serial.print("  \tZ:  "); Serial.print(lis.z); 
+  // Print measurements to serial monitor of Arduino
+//  Serial.print("X:  "); Serial.print(lis.x); 
+//  Serial.print("  \tY:  "); Serial.print(lis.y); 
+//  Serial.print("  \tZ:  "); Serial.println(lis.z);
+
+  // Transmit Measurements through Bluefruit module
+//  bluefruitSS.print("X:  "); bluefruitSS.print(lis.x); 
+//  bluefruitSS.print("  \tY:  "); bluefruitSS.print(lis.y); 
+//  bluefruitSS.print("  \tZ:  "); bluefruitSS.print(lis.z);
+//  bluefruitSS.println(1.00, HEX);
+//  Serial.println(1.00, HEX);
 
   /* Or....get a new sensor event, normalized */ 
   sensors_event_t event; 
   lis.getEvent(&event);
   
   /* Display the results (acceleration is measured in m/s^2) */
-  bluefruitSS.print("\t\tX: "); bluefruitSS.print(event.acceleration.x);
-  bluefruitSS.print(" \tY: "); bluefruitSS.print(event.acceleration.y); 
-  bluefruitSS.print(" \tZ: "); bluefruitSS.print(event.acceleration.z); 
-  bluefruitSS.println(" m/s^2 ");
+//  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
+//  Serial.print(" \tY: "); Serial.print(event.acceleration.y); 
+//  Serial.print(" \tZ: "); Serial.print(event.acceleration.z); 
+//  Serial.println(" m/s^2 ");
+//  bluefruitSS.println("test");
+//  bluefruitSS.println(event.acceleration.z, HEX);
+  bluefruitSS.println(lis.z, HEX);
+  Serial.println(lis.z);
+  // IMPORTANT: Need to put uart friend in command mode, use switch on board or AT commands
 
-  bluefruitSS.println();
+  /* Display the results (acceleration is measured in m/s^2) */
+//  bluefruitSS.print("\t\tX: "); bluefruitSS.print(event.acceleration.x);
+//  bluefruitSS.print(" \tY: "); bluefruitSS.print(event.acceleration.y); 
+//  bluefruitSS.print(" \tZ: "); bluefruitSS.print(event.acceleration.z); 
+//  bluefruitSS.println(" m/s^2 ");
+
+//  bluefruitSS.println();
+//  Serial.println();
  
-  delay(1);
+  delay(10); // time in milliseconds
 }
 
 /**************************************************************************/

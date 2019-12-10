0b617240e59a730877d5c84d4e1eccd66fb61755
mfmceneaney <50933545+mfmceneaney@users.noreply.github.com>
2019-12-09 20:48:27 -0500

    Create testModule.py

---
+++ modules/testModule.py
@@ -0,0 +1,14 @@
+#You can import any required modules here
+
+#This can be anything you want
+moduleName = "testModule"
+
+#All of the words must be heard in order for this module to be executed
+commandWords = ["start","stop","frequency","length","point","zero","one","two","three","four","five","six",
+            "seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen",
+            "eighteen","nineteen","twenty","thirty","forty","fifty"]
+
+def execute(command):
+    #Write anything you want to be executed when the commandWords are heard
+    #The 'command' parameter is the command you speak
+    return
